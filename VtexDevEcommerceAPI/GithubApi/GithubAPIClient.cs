fca82f22e6dcff495ee90cee91b621b1305d34f0
alexrc <alexrc.tecnologia@gmail.com>
2016-06-29 17:23:11 -0300

    Criação do server

---
+++ VtexDevEcommerceAPI/GithubApi/GithubAPIClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.Collections;
+using VtexDevEcommerceAPI.Entities;
+
+namespace VtexDevEcommerceAPI.GithubApi
+{
+    public class GithubAPIClient
+    {
+        HttpWebRequest req;
+
+        private void CreateRequest(string url)
+        {
+            req = (HttpWebRequest)WebRequest.Create(url);
+            req.Headers[HttpRequestHeader.Authorization] = "Basic YWxleHJjOmFwdGl2YTEy";
+            req.UserAgent = "VTex_Test";
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            CreateRequest("https://api.github.com/users");
+            string jsonResponse;
+            try
+            {
+                using (StreamReader responseReader = new StreamReader(req.GetResponse().GetResponseStream()))
+                    jsonResponse = responseReader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            
+            return JsonConvert.DeserializeObject<IEnumerable<User>>(jsonResponse);
+        }
+    }
+}
---
+++ VtexDevEcommerceAPI/IDevEcommerceService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using VtexDevEcommerceAPI;
+using VtexDevEcommerceAPI.Entities;
+
+
+namespace VtexDevEcommerceAPI
+{
+    [ServiceContract]
+    public interface IDevEcommerceService
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+                   ResponseFormat = WebMessageFormat.Json,
+                    RequestFormat = WebMessageFormat.Json,
+                        BodyStyle = WebMessageBodyStyle.Wrapped,
+                      UriTemplate = "GetUser/{id}")]
+        User GetUser(string id);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+           ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+                BodyStyle = WebMessageBodyStyle.Wrapped,
+              UriTemplate = "GetUsers")]
+        IEnumerable<User> GetUsers();
+    }
+}
